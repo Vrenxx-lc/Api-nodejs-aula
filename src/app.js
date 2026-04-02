7d0429cbfa7c808d3446dc7acafe449bdf59ae2a
Vrenxx-lc <saraiva.lucas13252@gmail.com>
2026-04-02 13:18:36 -0300

    atualização no app.js

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/teste', function(req, res){
+app.get('/', function(req, res){
 
     res.send("Ola mundo");
 
 
 })
+
+app.post('/pessoa', function (req, res){
+    console.log(req);
+    res.json({
+        "statusCode":200
+    })
+
+
+});
 
 app.listen(3000, ()=>console.log("Api Rodando."));
 
