a8ffec266d407d9706233238418024f35bc5ad00
Vrenxx-lc <saraiva.lucas13252@gmail.com>
2026-04-03 20:56:28 -0300

    api com crud finalizada

--- src/app.js
+++ src/app.js
@@ -1,59 +1,18 @@
-//gerando um ola mundo
-// import {openDb} from './configDB.js'
-// import { createTable,insertPessoa,updatePessoa, selectPessoas, selectPessoa, deletePessoa } from "./Controller/Pessoa.js";
-
 import express from "express";
+import fs from "fs";
+import https from "https";
+import cors from "cors";
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 import router from "./routes.js";
 app.use(router);
 
-// createTable();
+app.listen(3000, ()=> console.log("Api Rodando."));
 
-// app.get("/", function (req, res) {
-//   res.send("Api Rodando");
-// });
-
-// app.get("/pessoas", async function (req, res) {
-//   let pessoas = await selectPessoas();
-//    res.json(pessoas);
- 
-// });
-
-// app.get("/pessoa", async function (req, res) {
-//   let pessoa = await selectPessoa(req.body.id);
-//    res.json(pessoa);
- 
-// });
-
-
-// app.post("/pessoa", function (req, res) {
-//    insertPessoa(req.body);
-//    res.json({
-//      statusCode: 200
-//    });
-// });
-
-// app.put("/pessoa", function (req, res) {
-//   if (req.body && !req.body.id) {
-//     res.json({
-//       statusCode: 400,
-//       msg: "Voce precisa um id",
-//     });
-//   } else {
-//     updatePessoa(req.body);
-//     res.json({
-//       statusCode: 200,
-//     });
-//   }
-// });
-
-// app.delete("/pessoa", async function (req, res) {
-//   let pessoa = await deletePessoa(req.body.id);
-//    res.json(pessoa);
- 
-// });
-
-
-app.listen(3000, () => console.log("Api Rodando."));
+https.createServer({
+    cert: fs.readFileSync("src/SSL/code.crt"),
+    key: fs.readFileSync("src/SSL/code.key")
+}, app).listen(3001, () => console.log("Rodando em https."));
